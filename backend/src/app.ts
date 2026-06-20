ea26198b64e504606dc515f74aac62a769e960de
Ismail M <muhammedismailkt@gmail.com>
2026-06-20 19:37:48 +0530

    track package-lock.json to resolve Render build dependency caching1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,31 +1,31 @@
 import express from "express"
-// import cors from "cors"
+import cors from "cors"
 import restaurantRoutes from "./modules/restaurant/restaurant.routes"
 import { errorMiddleware } from "./shared/errors/error.middleware"
 
 const app = express()
 
-// app.use(
-//   cors({
-//     origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-//       // Allow requests with no origin (like mobile apps, curl, or server-side calls)
-//       if (!origin) return callback(null, true);
+app.use(
+  cors({
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+      // Allow requests with no origin (like mobile apps, curl, or server-side calls)
+      if (!origin) return callback(null, true);
 
-//       const allowedOrigins = [
-//         "https://restaurant-listing-nine.vercel.app"
-//       ];
+      const allowedOrigins = [
+        "https://restaurant-listing-nine.vercel.app"
+      ];
 
-//       // Allow any local origin (e.g. http://localhost:5173 or http://127.0.0.1:5174)
-//       const isLocal = /^http:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin);
+      // Allow any local origin (e.g. http://localhost:5173 or http://127.0.0.1:5174)
+      const isLocal = /^http:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin);
 
-//       if (allowedOrigins.includes(origin) || isLocal) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     }
-//   })
-// );
+      if (allowedOrigins.includes(origin) || isLocal) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    }
+  })
+);
 app.use(express.json())
 
 // Mount modules
